db49c976fd0008c6e2fa21fc05ba6fbef02f0166
Carlos <cnfcontato@protonmail.ch>
2021-09-08 22:10:43 -0300

    criação MVC via CMD

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Desenvolvimento_de_Software.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
